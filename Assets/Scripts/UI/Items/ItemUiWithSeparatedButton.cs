12fe2821ea4edeb8c5788db152f9114bc13a56e3
Viktor Repin <hiddenstmail@gmail.com>
2020-09-22 01:05:54 +0700

    IItemUi implementation

---
+++ Assets/Scripts/UI/Items/ItemUiWithSeparatedButton.cs
@@ -0,0 +1,23 @@
+﻿using InventorySystem.NewInventorySystem;
+using UnityEngine;
+
+namespace UI.Items {
+
+  public class ItemUiWithSeparatedButton : MonoBehaviour, IItemUi {
+    public GameObject itemImage;
+    public GameObject button;
+    
+    public GameObject SetItemButton(Transform position) {
+      return Instantiate(button, position);
+    }
+
+    public GameObject SetItemImage(Transform position) {
+      return Instantiate(itemImage, position);
+    }
+
+    public Sprite GetItemImage() {
+      return itemImage.GetComponent<Sprite>();
+    }
+  }
+
+}
---
+++ Assets/Scripts/UI/Items/ItemUiWithSingleButton.cs
@@ -0,0 +1,24 @@
+﻿using InventorySystem.NewInventorySystem;
+using UnityEngine;
+
+namespace UI.Items {
+
+  public class NewBehaviourScript : MonoBehaviour, IItemUi {
+    public GameObject itemButton;
+    private GameObject _instantiatedItemButton;
+    
+    public GameObject SetItemButton(Transform position) {
+      _instantiatedItemButton = Instantiate(itemButton, position);
+      return _instantiatedItemButton;
+    }
+
+    public GameObject SetItemImage(Transform position) {
+      return _instantiatedItemButton;
+    }
+
+    public Sprite GetItemImage() {
+      return itemButton.GetComponent<Sprite>();
+    }
+  }
+
+}
