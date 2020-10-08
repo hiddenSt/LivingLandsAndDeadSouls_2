11fe16758d47c633ca79e182f2569d2d7cd9ec5d
Viktor Repin <hiddenstmail@gmail.com>
2020-10-09 00:31:31 +0700

    Bad code

--- Assets/Scripts/UI/Controls/DropItem.cs
+++ Assets/Scripts/UI/Controls/DropItem.cs
@@ -2,28 +2,71 @@
 using UnityEngine.EventSystems;
 
 namespace UI.Controls {
-  
-  public class DropItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler {
+
+  public class DropItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler,
+    IDragHandler {
     public int slotIndex;
+    public float longTouchTime;
+    public DestroyCanvasControl destroyCanvasControl;
     private InventoryUi _inventoryUi;
-    private bool _dropped;
-    
-    public void OnPointerDown(PointerEventData eventData) { }
+    private bool _isDestroying;
+
+    public void OnPointerDown(PointerEventData eventData) {
+      if (gameObject.transform.childCount <= 0) {
+        _isDestroying = false;
+        return;
+      }
+      DetectLongTouch();
+    }
 
     public void OnBeginDrag(PointerEventData eventData) { }
 
-    public void OnDrag(PointerEventData eventData) { }
-    
+    public void OnDrag(PointerEventData eventData) {
+      Debug.Log("I am drag");
+    }
+
+    public void DestroyItem() {
+      _inventoryUi.RemoveItem(slotIndex);
+      _isDestroying = false;
+      DeactivateDestroyCanvas();
+    }
+
+    public void DontDestroyItem() {
+      _isDestroying = false;
+      DeactivateDestroyCanvas();
+    }
+
     public void OnEndDrag(PointerEventData eventData) {
-      _dropped = true;
       if (gameObject.transform.childCount <= 0)
         return;
-      _inventoryUi.DropItem(slotIndex);
+      if (!_isDestroying) {
+        _inventoryUi.DropItem(slotIndex);
+      }
+    }
+
+    private void DetectLongTouch() {
+      float holdTime = 0f;
+      while (holdTime < longTouchTime) {
+        holdTime += Time.deltaTime;
+      }
+      _isDestroying = true;
+      ActivateDestroyCanvas();
+    }
+    
+    private void ActivateDestroyCanvas() {
+      destroyCanvasControl.gameObject.SetActive(true);
+      destroyCanvasControl.noButton.onClick.AddListener(DontDestroyItem);
+      destroyCanvasControl.yesButton.onClick.AddListener(DestroyItem);
+    }
+
+    private void DeactivateDestroyCanvas() {
+      destroyCanvasControl.noButton.onClick.RemoveListener(DontDestroyItem);
+      destroyCanvasControl.yesButton.onClick.RemoveListener(DestroyItem);
+      destroyCanvasControl.gameObject.SetActive(false);
     }
     
     private void Start() {
       _inventoryUi = transform.parent.gameObject.GetComponent<InventoryUi>();
-      _dropped = false;
     }
   }
   
